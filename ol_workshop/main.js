953724db5f6ab8d9e8af4fce838ba214da87afdc
bykolyv <stu2201262036@uni-plovdiv.bg>
2024-03-14 01:45:52 +0200

    Add clear features and serialize feature data buttons.

--- ol_workshop/main.js
+++ ol_workshop/main.js
@@ -41,3 +41,16 @@ map.addInteraction(
   })
 );
 map.addInteraction(new Link());
+
+const clear = document.getElementById('clear');
+clear.addEventListener('click', function () {
+  source.clear();
+});
+
+const format = new GeoJSON({featureProjection: 'EPSG:3857'});
+const download = document.getElementById('download');
+source.on('change', function () {
+  const features = source.getFeatures();
+  const json = format.writeFeatures(features);
+  download.href = 'data:application/json;charset=utf-8,' + encodeURIComponent(json);
+});
