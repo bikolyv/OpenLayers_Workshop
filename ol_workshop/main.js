435a89db23f8062b965ba1fd7da69d62cac41450
bykolyv <stu2201262036@uni-plovdiv.bg>
2024-03-15 00:01:07 +0200

    Add user's location.

--- ol_workshop/main.js
+++ ol_workshop/main.js
@@ -1,56 +1,41 @@
 import './style.css';
 import {Map, View} from 'ol';
+
 import GeoJSON from 'ol/format/GeoJSON';
 import VectorLayer from 'ol/layer/Vector';
 import VectorSource from 'ol/source/Vector';
+import OSM from 'ol/source/OSM';
+import TileLayer from 'ol/layer/Tile';
+import {fromLonLat} from 'ol/proj';
+
 import {Link, DragAndDrop, Modify, Draw, Snap}  from 'ol/interaction';
 import {Style, Fill, Stroke} from 'ol/style';
 
 import colormap from 'colormap';
 import { getArea } from 'ol/sphere';
 
-const steps = 50;
-const ramp = colormap({
-  colormap: 'blackbody',
-  nshades: steps,
+import Feature from 'ol/Feature';
+import Point from 'ol/geom/Point';
+import { circular } from 'ol/geom/Polygon';
+import Control from 'ol/control/Control';
+
+const map = new Map({
+  target: 'map-container',
+  layers: [
+    new TileLayer({
+      source: new OSM(),
+    }),
+  ],
+  view: new View({
+    center: fromLonLat([0, 0]),
+    zoom: 2,
+  }),
 });
-
-const min = 1e8;
-const max = 2e13;
-function clamp(value, low, high) {
-  return Math.max(low, Math.min(value, high));
-}
-
-function getColor(feature) {
-  const area = getArea(feature.getGeometry());
-  const f = Math.pow(clamp((area - min) / (max - min), 0, 1), 1/2);
-  const index  = Math.round(f * (steps - 1));
-  return ramp[index];
-}
-
 
 const source = new VectorSource();
 
 const layer = new VectorLayer({
   source: source,
-  style: function(feature) {
-    return new Style({
-      fill: new Fill({
-        color: getColor(feature),
-      }),
-      stroke: new Stroke({
-        color: 'rgba(255,255,255,0.8)',
-      }),
-    });
-  },
-});
-
-const map = new Map({
-  target: 'map-container',
-  view: new View({
-    center: [0, 0],
-    zoom: 2
-  })
 });
 
 map.addLayer(layer);
@@ -77,6 +62,43 @@ map.addInteraction(
   })
 );
 map.addInteraction(new Link());
+
+navigator.geolocation.watchPosition(
+  function(pos) {
+    const coords = [pos.coords.longitude, pos.coords.latitude];
+    const accuracy = circular(coords, pos.coords.accuracy);
+    source.clear(true);
+    source.addFeatures([
+      new Feature(
+        accuracy.transform('EPSG:4326', map.getView().getProjection())
+      ),
+      new Feature(new Point(fromLonLat(coords))),
+    ]);
+  },
+  function(error) {
+    alert('ERROR: ${error.message}');
+  },
+  {
+    enableHighAccuracy: true,
+  }
+)
+
+const locate = document.createElement('div');
+locate.className = 'ol-control ol-unselectable locate';
+locate.innerHTML = '<button title="Locate me">◎</button>';
+locate.addEventListener('click', function() {
+  if(!source.isEmpty()) {
+    map.getView().fit(source.getExtent(), {
+      maxZoom: 18,
+      duration: 500,
+    });
+  }
+});
+map.addControl(
+  new Control({
+    element: locate,
+  })
+);
 
 const clear = document.getElementById('clear');
 clear.addEventListener('click', function () {
