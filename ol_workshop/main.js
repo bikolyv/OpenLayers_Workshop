c07359fe7be9c0596467b209f3881296e321de90
bykolyv <stu2201262036@uni-plovdiv.bg>
2024-03-15 16:33:18 +0200

    Render false color composite.

--- ol_workshop/main.js
+++ ol_workshop/main.js
@@ -8,7 +8,19 @@ import {getCenter} from 'ol/extent.js';
 const source = new GeoTIFF({
   sources: [
     {
-      url: 'https://sentinel-cogs.s3.us-west-2.amazonaws.com/sentinel-s2-l2a-cogs/21/H/UB/2021/9/S2B_21HUB_20210915_0_L2A/TCI.tif',
+      // near-infrared reflectance
+      url: 'https://sentinel-cogs.s3.us-west-2.amazonaws.com/sentinel-s2-l2a-cogs/21/H/UB/2021/9/S2B_21HUB_20210915_0_L2A/B08.tif',
+      max: 5000,
+    },
+    {
+      // red reflectance
+      url: 'https://sentinel-cogs.s3.us-west-2.amazonaws.com/sentinel-s2-l2a-cogs/21/H/UB/2021/9/S2B_21HUB_20210915_0_L2A/B04.tif',
+      max: 5000,
+    },
+    {
+      // green reflectance
+      url: 'https://sentinel-cogs.s3.us-west-2.amazonaws.com/sentinel-s2-l2a-cogs/21/H/UB/2021/9/S2B_21HUB_20210915_0_L2A/B03.tif',
+      max: 5000,
     },
   ],
 });
