56970757705f6cd49bd450ff4a5906a09453b55a
bykolyv <stu2201262036@uni-plovdiv.bg>
2024-03-15 18:51:40 +0200

    Add selecting imagery source.

--- ol_workshop/main.js
+++ ol_workshop/main.js
@@ -83,16 +83,43 @@ visualizations.forEach((visualization) => {
   visualizationSelector.appendChild(option);
 });
 
+const images = [
+  {
+    name: 'Buenos Aires',
+    base: 'https://sentinel-cogs.s3.us-west-2.amazonaws.com/sentinel-s2-l2a-cogs/21/H/UB/2021/9/S2B_21HUB_20210915_0_L2A',
+  },
+  {
+    name: 'Minneapolis',
+    base: 'https://sentinel-cogs.s3.us-west-2.amazonaws.com/sentinel-s2-l2a-cogs/15/T/WK/2021/9/S2B_15TWK_20210918_0_L2A',
+  },
+  {
+    name: 'Cape Town',
+    base: 'https://sentinel-cogs.s3.us-west-2.amazonaws.com/sentinel-s2-l2a-cogs/34/H/BH/2021/9/S2B_34HBH_20210922_0_L2A',
+  },
+];
+
+const imageSelector = document.getElementById('image');
+images.forEach((image) => {
+  const option = document.createElement('option');
+  option.textContent = image.name;
+  imageSelector.appendChild(option);
+})
+
+let previousBase;
 function updateVisualization() {
   const visualization = visualizations[visualizationSelector.selectedIndex];
-  const base =
-    'https://sentinel-cogs.s3.us-west-2.amazonaws.com/sentinel-s2-l2a-cogs/21/H/UB/2021/9/S2B_21HUB_20210915_0_L2A';
+  const base = images[imageSelector.selectedIndex].base;
+  const newBase = base !== previousBase;
+  previousBase = base;
 
   const layer = createLayer(base, visualization);
   map.setLayers([layer]);
 
-  map.setView(layer.getSource().getView());
+  if(newBase) {
+    map.setView(layer.getSource().getView());
+  }
 }
 
 visualizationSelector.addEventListener('change', updateVisualization);
+imageSelector.addEventListener('change', updateVisualization);
 updateVisualization();
