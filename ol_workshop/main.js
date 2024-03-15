9f3d4203f57d80782d4c1a0e13915db07c3f31a5
bykolyv <stu2201262036@uni-plovdiv.bg>
2024-03-15 15:15:45 +0200

    Simplify view

--- ol_workshop/main.js
+++ ol_workshop/main.js
@@ -5,14 +5,6 @@ import TileLayer from 'ol/layer/WebGLTile.js';
 import View from 'ol/View.js';
 import {getCenter} from 'ol/extent.js';
 
-const projection = new Projection({
-  code: 'EPSG:32721',
-  units: 'm',
-});
-
-// metadata from https://s3.us-west-2.amazonaws.com/sentinel-cogs/sentinel-s2-l2a-cogs/21/H/UB/2021/9/S2B_21HUB_20210915_0_L2A/S2B_21HUB_20210915_0_L2A.json
-const sourceExtent = [300000, 6090260, 409760, 6200020];
-
 const source = new GeoTIFF({
   sources: [
     {
@@ -28,10 +20,5 @@ const layer = new TileLayer({
 new Map({
   target: 'map-container',
   layers: [layer],
-  view: new View({
-    projection: projection,
-    center: getCenter(sourceExtent),
-    extent: sourceExtent,
-    zoom: 1,
-  }),
+  view: source.getView(),
 });
