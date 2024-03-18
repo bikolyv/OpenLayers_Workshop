d9cb71d936bf7a379ffc15e462687abbdcd4daf2
bykolyv <stu2201262036@uni-plovdiv.bg>
2024-03-18 23:13:38 +0200

    Map setup

--- ol_workshop/main.js
+++ ol_workshop/main.js
@@ -1,55 +1,60 @@
-import MVT from 'ol/format/MVT';
-import VectorTileLayer from 'ol/layer/VectorTile';
-import VectorTileSource from 'ol/source/VectorTile';
-import {Map, View} from 'ol';
-import {fromLonLat} from 'ol/proj';
-import VectorLayer from 'ol/layer/Vector';
-import VectorSource from 'ol/source/Vector';
-import {Stroke, Style} from 'ol/style';
 import Feature from 'ol/Feature';
-import {fromExtent} from 'ol/geom/Polygon';
-
-const map = new Map({
-  target: 'map-container',
-  view: new View({
-    center: fromLonLat([0, 0]),
-    zoom: 2,
-  }),
-});
-
-const layer = new VectorTileLayer({
-  source: new VectorTileSource({
-    format: new MVT(),
-    url:
-      'https://ahocevar.com/geoserver/gwc/service/tms/1.0.0/' +
-      'ne:ne_10m_admin_0_countries@EPSG%3A900913@pbf/{z}/{x}/{-y}.pbf',
-    maxZoom: 14,
-  }),
-});
-map.addLayer(layer);
+import Point from 'ol/geom/Point';
+import TileLayer from 'ol/layer/Tile';
+import VectorLayer from 'ol/layer/Vector';
+import {Map, View} from 'ol';
+import {Stamen, Vector as VectorSource} from 'ol/source';
+import {fromLonLat} from 'ol/proj';
 
 const source = new VectorSource();
-new VectorLayer({
-  map: map,
+
+const client = new XMLHttpRequest();
+client.open('GET', './data/meteorites.csv');
+client.onload = function () {
+  const csv = client.responseText;
+  const features = [];
+
+  let prevIndex = csv.indexOf('\n') + 1; // scan past the header line
+
+  let curIndex;
+  while ((curIndex = csv.indexOf('\n', prevIndex)) != -1) {
+    const line = csv.substr(prevIndex, curIndex - prevIndex).split(',');
+    prevIndex = curIndex + 1;
+
+    const coords = fromLonLat([parseFloat(line[4]), parseFloat(line[3])]);
+    if (isNaN(coords[0]) || isNaN(coords[1])) {
+      // guard against bad data
+      continue;
+    }
+
+    features.push(
+      new Feature({
+        mass: parseFloat(line[1]) || 0,
+        year: parseInt(line[2]) || 0,
+        geometry: new Point(coords),
+      })
+    );
+  }
+  source.addFeatures(features);
+};
+client.send();
+
+const meteorites = new VectorLayer({
   source: source,
-  style: new Style({
-    stroke: new Stroke({
-      color: 'red',
-      width: 4,
-    }),
-  }),
 });
 
-map.on('pointermove', function (event) {
-  source.clear();
-  map.forEachFeatureAtPixel(
-    event.pixel,
-    function (feature) {
-      const geometry = feature.getGeometry();
-      source.addFeature(new Feature(fromExtent(geometry.getExtent())));
-    },
-    {
-      hitTolerance: 2,
-    }
-  );
+new Map({
+  target: 'map-container',
+  layers: [
+    new TileLayer({
+      source: new Stamen({
+        layer: 'toner',
+      }),
+    }),
+    meteorites,
+  ],
+  view: new View({
+    center: [0, 0],
+    zoom: 2,
+  }),
 });
