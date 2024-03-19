07f4523ddc834648bded5f4a28e92f456aaa4383
bykolyv <stu2201262036@uni-plovdiv.bg>
2024-03-19 22:27:10 +0200

    Render Sea Level

--- ol_workshop/main.js
+++ ol_workshop/main.js
@@ -1,74 +1,78 @@
-import Feature from 'ol/Feature';
-import Point from 'ol/geom/Point';
-import TileLayer from 'ol/layer/Tile';
-import VectorLayer from 'ol/layer/Vector';
-import {Map, View} from 'ol';
-import {Stamen, Vector as VectorSource} from 'ol/source';
-import {fromLonLat} from 'ol/proj';
-import WebGLPointsLayer from 'ol/layer/WebGLPoints';
+import Map from 'ol/Map.js';
+import TileLayer from 'ol/layer/WebGLTile.js';
+import View from 'ol/View.js';
+import XYZ from 'ol/source/XYZ.js';
+import {fromLonLat} from 'ol/proj.js';
+const key = 'tdaYsuIlhqhN2h25ZDPI';
 
-const source = new VectorSource();
+const attributions =
+  '<a href="https://www.maptiler.com/copyright/" target="_blank">&copy; MapTiler</a> ' +
+  '<a href="https://www.openstreetmap.org/copyright" target="_blank">&copy; OpenStreetMap contributors</a>';
 
-const client = new XMLHttpRequest();
-client.open('GET', './data/meteorites.csv');
-client.onload = function () {
-  const csv = client.responseText;
-  const features = [];
-
-  let prevIndex = csv.indexOf('\n') + 1; // scan past the header line
-
-  let curIndex;
-  while ((curIndex = csv.indexOf('\n', prevIndex)) != -1) {
-    const line = csv.substr(prevIndex, curIndex - prevIndex).split(',');
-    prevIndex = curIndex + 1;
-
-    const coords = fromLonLat([parseFloat(line[4]), parseFloat(line[3])]);
-    if (isNaN(coords[0]) || isNaN(coords[1])) {
-      // guard against bad data
-      continue;
-    }
-
-    features.push(
-      new Feature({
-        mass: parseFloat(line[1]) || 0,
-        year: parseInt(line[2]) || 0,
-        geometry: new Point(coords),
-      })
-    );
-  }
-  source.addFeatures(features);
-};
-client.send();
-
-const meteorites = new WebGLPointsLayer({
-  source: source,
-  style: {
-    symbol: {
-      symbolType: 'circle',
-      size: [
-        '+',
-        ['*', ['clamp', ['*', ['get', 'mass'], 1 / 20000], 0, 1], 18],
-        8,
-      ],      
-      color: 'rgb(255, 0, 0)',
-      opacity: 0.5,
-    },
-  },
-});
-
-
-new Map({
+const map = new Map({
   target: 'map-container',
   layers: [
     new TileLayer({
-      source: new Stamen({
-        layer: 'toner',
+      source: new XYZ({
+        url: 'https://api.maptiler.com/maps/streets/{z}/{x}/{y}.png?key=' + key,
+        attributions: attributions,
+        crossOrigin: 'anonymous',
+        tileSize: 512,
       }),
     }),
-    meteorites,
   ],
   view: new View({
-    center: [0, 0],
-    zoom: 2,
+    center: fromLonLat([-58.3816, -34.6037]),
+    zoom: 11,
   }),
 });
+
+// band math operates on normalized values from 0-1
+// so we scale by 255 to align with the elevation formula
+// from https://cloud.maptiler.com/tiles/terrain-rgb/
+const elevation = [
+  '+',
+  -10000,
+  [
+    '*',
+    0.1 * 255,
+    [
+      '+',
+      ['*', 256 * 256, ['band', 1]],
+      ['+', ['*', 256, ['band', 2]], ['band', 3]],
+    ],
+  ],
+];
+
+const layer = new TileLayer({
+  opacity: 0.6,
+  source: new XYZ({
+    url:
+      'https://api.maptiler.com/tiles/terrain-rgb/{z}/{x}/{y}.png?key=' + key,
+    maxZoom: 10,
+    tileSize: 512,
+    crossOrigin: 'anonymous',
+  }),
+  style: {
+    variables: {
+      level: 0,
+    },
+    color: [
+      'case',
+      ['<=', elevation, ['var', 'level']],
+      [139, 212, 255, 1],
+      [139, 212, 255, 0],
+    ],
+  },
+});
+map.addLayer(layer);
+
+const control = document.getElementById('level');
+const output = document.getElementById('output');
+const listener = function () {
+  output.innerText = control.value;
+  layer.updateStyleVariables({level: parseFloat(control.value)});
+};
+control.addEventListener('input', listener);
+control.addEventListener('change', listener);
+output.innerText = control.value;
